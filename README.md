3925731a1a81315772435b4c5a61dc1065675845
Hydra <sakthipraba04+01@gmail.com>
2023-10-23 13:13:49 +0800

    Refactoring

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# HydraOS
-An Operating System created in C, C++ and Assembly
+# Basic Operating System in C, C++, Assembly, and SCons
+
+## Introduction
+
+In this project, we will develop a basic operating system using a combination of programming languages, including C, C++, Assembly, and build automation with SCons. This project aims to provide a fundamental understanding of operating system development and the integration of various languages and tools.
+
+## Technologies Used
+
+- **C**: Used for low-level system programming and core functionality implementation.
+- **C++**: Employed for object-oriented programming and higher-level system components.
+- **Assembly**: Utilized for low-level hardware interactions and optimizing critical sections of the code.
+- **SCons**: A modern build automation tool, simplifying the build process and managing dependencies.
+
+## Project Structure
+
+```markdown
+- 📂 **src**
+  - 📄 kernel.c (Core kernel functionality in C)
+  - 📄 kernel.hpp (Kernel declarations and class definitions in C++)
+  - 📄 bootloader.asm (Bootloader written in Assembly)
+- 📂 **build**
+  - 📄 SConstruct (SCons configuration file)
+- 📂 **bin**
+  - 📄 bootloader.bin (Compiled bootloader binary)
+  - 📄 kernel.bin (Compiled kernel binary)
+- 📂 **docs**
+  - 📄 design.md (Design documentation)
+  - 📄 usage.md (Instructions for building and running the OS)
+- 📄 README.md (Project overview and instructions)
+```
+
+## Implementation Details
+
+### Bootloader (Assembly)
+
+The bootloader, written in Assembly, is responsible for loading the operating system kernel into memory and transferring control to the kernel code.
+
+### Kernel (C and C++)
+
+- **kernel.c**: Contains low-level functionality such as memory management, hardware interactions, and system calls.
+- **kernel.hpp**: Includes class definitions and higher-level system components like process management, file system, and device drivers.
+
+## Build Process with SCons
+
+The build process is managed by SCons, ensuring efficient compilation and linking of source files. The `SConstruct` file in the `build` directory contains build configurations and dependencies.
+
+### Building the Project
+
+To build the project, execute the following command in the terminal:
+
+```bash
+scons
+```
+
+This command will compile the bootloader, kernel, and other necessary components, generating binary files in the `bin` directory.
+
+## Running the Operating System
+
+To run the operating system in an emulator or on real hardware, follow the instructions provided in the `docs/usage.md` file.
+
+## Conclusion
+
+This project demonstrates the development of a basic operating system using a combination of C, C++, Assembly, and SCons. By exploring this project, developers can gain insights into low-level system programming, language integration, and build automation, paving the way for more complex operating system projects and system-level optimizations.
---
+++ src/bootloader/stage1/boot.asm
@@ -0,0 +1,274 @@
+[org 0x7C00]
+[bits 16]
+
+
+%define ENDL 0x0D, 0x0A
+
+
+;
+; FAT12 header
+; 
+jmp short start
+nop
+
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
+bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+; extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
+ebr_volume_label:           db 'HYDRA    OS'        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
+
+;
+; Code goes here
+;
+
+start:
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+    
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00              ; stack grows downwards from where we are loaded in memory
+
+    ; some BIOSes might start us at 07C0:0000 instead of 0000:7C00, make sure we are in the
+    ; expected location
+    push es
+    push word .after
+    retf
+
+.after:
+
+    ; read something from floppy disk
+    ; BIOS should set DL to drive number
+    mov [ebr_drive_number], dl
+
+    ; show loading message
+    mov si, msg_loading
+    call puts
+
+    ; read drive parameters (sectors per track and head count),
+    ; instead of relying on data on formatted disk
+    push es
+    mov ah, 08h
+    int 13h
+    jc floppy_error
+    pop es
+
+    and cl, 0x3F                        ; remove top 2 bits
+    xor ch, ch
+    mov [bdb_sectors_per_track], cx     ; sector count
+
+    inc dh
+    mov [bdb_heads], dh                 ; head count
+
+    ; load stage2
+    
+
+.read_finish:
+
+    ; jump to our kernel
+    mov dl, [ebr_drive_number]          ; boot device in dl
+
+    mov ax, STAGE2_LOAD_SEGMENT         ; set segment registers
+    mov ds, ax
+    mov es, ax
+
+    jmp STAGE2_LOAD_SEGMENT:STAGE2_LOAD_OFFSET
+
+    jmp wait_key_and_reboot             ; should never happen
+
+    cli                                 ; disable interrupts, this way CPU can't get out of "halt" state
+    hlt
+
+
+;
+; Error handlers
+;
+
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+kernel_not_found_error:
+    mov si, msg_stage2_not_found
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h                     ; wait for keypress
+    jmp 0FFFFh:0                ; jump to beginning of BIOS, should reboot
+
+.halt:
+    cli                         ; disable interrupts, this way CPU can't get out of "halt" state
+    hlt
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+
+;
+; Disk routines
+;
+
+;
+; Converts an LBA address to a CHS address
+; Parameters:
+;   - ax: LBA address
+; Returns:
+;   - cx [bits 0-5]: sector number
+;   - cx [bits 6-15]: cylinder
+;   - dh: head
+;
+
+lba_to_chs:
+
+    push ax
+    push dx
+
+    xor dx, dx                          ; dx = 0
+    div word [bdb_sectors_per_track]    ; ax = LBA / SectorsPerTrack
+                                        ; dx = LBA % SectorsPerTrack
+
+    inc dx                              ; dx = (LBA % SectorsPerTrack + 1) = sector
+    mov cx, dx                          ; cx = sector
+
+    xor dx, dx                          ; dx = 0
+    div word [bdb_heads]                ; ax = (LBA / SectorsPerTrack) / Heads = cylinder
+                                        ; dx = (LBA / SectorsPerTrack) % Heads = head
+    mov dh, dl                          ; dh = head
+    mov ch, al                          ; ch = cylinder (lower 8 bits)
+    shl ah, 6
+    or cl, ah                           ; put upper 2 bits of cylinder in CL
+
+    pop ax
+    mov dl, al                          ; restore DL
+    pop ax
+    ret
+
+
+;
+; Reads sectors from a disk
+; Parameters:
+;   - ax: LBA address
+;   - cl: number of sectors to read (up to 128)
+;   - dl: drive number
+;   - es:bx: memory address where to store read data
+;
+disk_read:
+
+    push ax                             ; save registers we will modify
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx                             ; temporarily save CL (number of sectors to read)
+    call lba_to_chs                     ; compute CHS
+    pop ax                              ; AL = number of sectors to read
+    
+    mov ah, 02h
+    mov di, 3                           ; retry count
+
+.retry:
+    pusha                               ; save all registers, we don't know what bios modifies
+    stc                                 ; set carry flag, some BIOS'es don't set it
+    int 13h                             ; carry flag cleared = success
+    jnc .done                           ; jump if carry not set
+
+    ; read failed
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; all attempts are exhausted
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax                             ; restore registers modified
+    ret
+
+
+;
+; Resets disk controller
+; Parameters:
+;   dl: drive number
+;
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+
+
+msg_loading:            db 'Loading...', ENDL, 0
+msg_read_failed:        db 'Read from disk failed!', ENDL, 0
+msg_stage2_not_found:   db 'STAGE2.BIN file not found!', ENDL, 0
+file_stage2_bin:        db 'STAGE2  BIN'
+
+stage2_location:        times 30 db 0
+
+STAGE2_LOAD_SEGMENT     equ 0x0
+STAGE2_LOAD_OFFSET      equ 0x500
+
+
+times 510-($-$$) db 0
+dw 0AA55h
+
+buffer:
