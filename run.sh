76157a95a7a52c4cf67516ce20497d8fc06586d6
Hydra <sakthipraba04+01@gmail.com>
2023-10-28 11:51:48 +0800

    Refactoring

--- run.sh
+++ run.sh
@@ -1,20 +1,3 @@
-T=$(date +"%T")
-SavedIFS="$IFS"
-IFS=":."
-Time=($T)
-Seconds=$((${Time[0]}*3600 + ${Time[1]}*60 + ${Time[2]}))
-IFS="$SavedIFS"
-
-echo $Seconds
 make clean
 make
-
-T=$(date +"%T")
-SavedIFS="$IFS"
-IFS=":."
-Time=($T)
-Seconds2=$((${Time[0]}*3600 + ${Time[1]}*60 + ${Time[2]}))
-IFS="$SavedIFS"
-echo $Seconds
-
 qemu-system-i386 -fda build/main_floppy.img
